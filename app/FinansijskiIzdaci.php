5ed59b3315f8215e87ed25f8b1a51104f1afb25c
filipned <nedovicf@gmail.com>
2016-11-08 17:16:59 +0100

    Napravljena prva polovina modela

---
+++ app/FinansijskiIzdaci.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FinansijskiIzdaci extends Model
+{
+    //
+    protected $table = 'finansijskiizdaci';
+    protected $primaryKey = "finansijskiIzdaciID";
+}
---
+++ app/OpstiPodaci.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OpstiPodaci extends Model
+{
+    //
+    protected $table = 'opstipodaci';
+    protected $primaryKey = "opstiPodaciID";
+}
