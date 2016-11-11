dd046c97be4b08cb0581babbc2ad35b0a373a0c1
Marko Otasevic <motasevic994gmail.com>
2016-11-11 22:16:35 +0100

    Zakomentarisan javascrip, ceka se resenje..

---
+++ app/Http/Controllers/FormularController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormularController extends Controller
+{
+
+    public function postFormular(Request $request)
+    {
+        dd($request);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::post('/postFormular','FormularController@postFormular');
+
+Route::get('/', function(){
+	return view('Forma_Procedure');
+});
