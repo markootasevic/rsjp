eee8a899f7ea869602de288e44e9b2e513dd866f
Marko Otasevic <motasevic994gmail.com>
2016-11-14 11:49:06 +0100

    samo da bi pull

--- app/Delatnost.php
+++ app/Delatnost.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Delatnost extends Model
 {
-    // tabela
+    public $timestamps = false;
     protected $table = 'delatnost';
     protected $primaryKey = "delatnostRbr";
+     protected $fillable = [
+        'delatnost',
+     ];
 
     public function opstiPodaci()
     {
--- app/Poddelatnost.php
+++ app/Poddelatnost.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Poddelatnost extends Model
 {
-    //
+    public $timestamps = false;
     protected $table = 'poddelatnost';
     protected $primaryKey = "poddelatnostRbr";
+    protected $fillable = [
+    	
+    ];
 
     public function delatnost()
     {
